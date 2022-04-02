b577f0b6fe1ed4981aae96a82a34f9492687f2ff
caquinta <cquintana002@gmail.com>
2022-04-02 13:29:49 +0200

    commit toupper to lower

--- ft_tolower.c
+++ ft_tolower.c
@@ -5,29 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: caquinta <caquinta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/02 13:24:19 by caquinta          #+#    #+#             */
-/*   Updated: 2022/04/02 13:27:38 by caquinta         ###   ########.fr       */
+/*   Created: 2022/04/02 12:35:45 by caquinta          #+#    #+#             */
+/*   Updated: 2022/04/02 13:29:11 by caquinta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <unistd.h>
-int ft_tolower(int c);
+
+int ft_toupper(int c);
 
 int main()
 {
-	printf("Mi funcion da: %d\n", ft_tolower(97));
-	char a = ft_tolower(97);
+	printf("Mi funcion da: %d\n", ft_toupper(65));
+	char a = ft_toupper(65);
 	write(1, &a,1);
 	return(0);
 }
 
-int ft_tolower(int c)
+int ft_toupper(int c)
 {
-	if(c>=97 && c<=122)
+	if(c>=65 && c<=90)
 	{
-		c -=32;
+		c +=32;
 	}
 	return(c);
 }
