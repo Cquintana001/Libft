7ed1769bab30848353fdb8c1627a8182c9390958
caquinta <cquintana002@gmail.com>
2022-04-02 19:41:22 +0200

    final commit 02/04/2022

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,24 +6,62 @@
 /*   By: caquinta <caquinta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 10:54:51 by caquinta          #+#    #+#             */
-/*   Updated: 2022/04/02 18:57:20 by caquinta         ###   ########.fr       */
+/*   Updated: 2022/04/02 19:39:27 by caquinta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <memory.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 void	*ft_memmove(void *dst, const void *src, size_t len);
 
-/* int	main(void)
+int		main()
 {
-	char	src[]= "hola a todos";
-	char	dst[]= "";
+	char	src[] = "lorem ipsum dolor sit amet";
+	char	*dest;
+	 
 
-		
-	printf("Mi  funcion da: %s", ft_memmove(dst, src, 4));
-} */
+	dest = src + 1;
+	alarm(5);
+	 
+		/* if (dest != ft_memmove(dest, "consectetur", 5))
+			write(1, "dest's adress was not returned\n", 31);
+		write(1, dest, 22);
+		write(1, "\n",1);
+	
+	 
+	
+		if (dest != ft_memmove(dest, "con\0sec\0\0te\0tur", 10))
+			write(1, "dest's adress was not returned\n", 31);
+		write(1, dest, 22);
+		write(1, "\n",1);
+	
+	 
+	
+		if (dest != ft_memmove(dest, src, 8))
+			write(1, "dest's adress was not returned\n", 31);
+		write(1, dest, 22);
+		write(1, "\n",1);
+	
+	  */
+	
+		/* if (src != ft_memmove(src, dest, 8))
+			write(1, "dest's adress was not returned\n", 31);
+		write(1, dest, 22);
+		write(1, "\n",1); */
+	
+	 
+	
+		if (src != ft_memmove(src, dest, 0))
+			write(1, "dest's adress was not returned\n", 31);
+		write(1, dest, 22);
+	
+	return (0);
+}
+
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char	*array;
