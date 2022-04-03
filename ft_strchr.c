caa15f21b5d6813723b9d6734b382ef7af64442e
caquinta <cquintana002@gmail.com>
2022-04-03 13:12:08 +0200

    2nd commit 03/04/2022

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,20 +6,20 @@
 /*   By: caquinta <caquinta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 13:32:21 by caquinta          #+#    #+#             */
-/*   Updated: 2022/04/03 11:30:11 by caquinta         ###   ########.fr       */
+/*   Updated: 2022/04/03 13:10:07 by caquinta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
-char *ft_strchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
 
 /* int main()
 {
 	char array[] = "Hola";
 
-	int c = 'o';
+	int c = 0;
 
 	printf("Mi funcion da: %s", strchr(array, c));
 
@@ -27,18 +27,23 @@ char *ft_strchr(const char *s, int c);
 
 } */
 
-char *ft_strchr(const char *s, int c)
-{	int x;
- 
+char	*ft_strchr(const char *s, int c)
+{
+	int	x;
 
-	x =0;
-	while(s[x])
+	x = 0;
+	while (s[x])
 	{
-		if(s[x] == c)
-		{	 
-			return((char *)&s[x]);
+		if (s[x] == c)
+		{
+			return ((char *)&s[x]);
 		}
-	x++;
+		x++;
 	}
-	return(NULL);
+	x = 0;
+	while (s[x])
+		x++;
+	if (c == 0)
+		return ((char *) &s[x]);
+	return (NULL);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,36 +6,47 @@
 /*   By: caquinta <caquinta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 13:59:26 by caquinta          #+#    #+#             */
-/*   Updated: 2022/04/02 18:09:30 by caquinta         ###   ########.fr       */
+/*   Updated: 2022/04/03 13:05:59 by caquinta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include <stdio.h>
+
 char	*ft_strrchr(const char *s, int c);
+
 /* int main()
 {
-	char array[] = "Hola";
+	 char            *str;
+        char            str2[] = "bonjour";
+        
 
-	int c = 'o';
+		if (!(str = ft_strrchr(str2 + 2, 'b')))
+                        printf("NULL");
+                else
+                        printf("%s",str);
 
-	printf("Mi funcion da: %s", strchr(array, c));
-
-	return(0);
+	 
 
 } */
 
 char	*ft_strrchr(const char *s, int c)
-{	
+{
 	int	x;
 
 	x = 0;
 	while (s[x])
+		x++;
+	if (c == 0)
+		return ((char *) &s[x]);
+	x--;
+	while (x >= 0)
 	{
 		if (s[x] == c)
 		{
-			return ((char *) & s[x]);
+			return ((char *)&s[x]);
 		}
-	x++;
+		x--;
 	}
 	return (NULL);
 }
