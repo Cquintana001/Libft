e40e575a4d78ef4fb393535130c62f1ebda0b598
caquinta <cquintana002@gmail.com>
2022-04-06 10:55:29 +0000

    ft_strlcat working without norminette

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 19:19:01 by caquinta          #+#    #+#             */
-/*   Updated: 2022/04/06 10:06:05 by user             ###   ########.fr       */
+/*   Updated: 2022/04/06 10:54:29 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,20 @@
 size_t ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize);
 size_t ft_strlen(const char *s);
 
-int main()
+/*int main()
 {
 	char dest[15] = "rrrrrr";
-	dest[11] = 'a';
+	dest[14] = 'a';
 	
-	printf("La funcion original da: %zu\n", strlcat(dest, "lorem", 15));
+	printf("La funcion original da: %zu\n", strlcat(dest, "lorem ipsum dolor sit amet", 15));
 
 	printf("dest da: %s\n", dest);
 	 
 	
 	char dest1[15] = "rrrrrr";
-	dest1[11] = 'a';
+	dest1[14] = 'a';
 	
-	printf("Mi funcion da: %zu\n", ft_strlcat(dest1, "lorem", 15));
+	printf("Mi funcion da: %zu\n", ft_strlcat(dest1, "lorem ipsum dolor sit amet", 15));
 
 	printf("dest da: %s\n\n", dest1);
 
@@ -39,8 +39,8 @@ int main()
 	
 
 
+}*/
 
-}
 
 size_t ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize)
 {
@@ -51,29 +51,19 @@ size_t ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize
 
 	srclen = ft_strlen(src);
 	dstlen = ft_strlen(dst);
-	printf("la longitud de src es %lu\n", srclen);
-	printf("la longitud de dst es %lu\n", dstlen);
-
-
 	x = dstlen;
 	y = 0;
-	if (dstsize != 0 && src[y])
-	{
-		while (src[y] && x < dstsize - 1 )
+		while (src[y] && x < dstsize - 1 && dstsize!=0 )
 		{
 			dst[x] = src[y];
 			x++;
 			y++;
 		}
-		if(x < dstsize - 1)
-			dst[x] = '\0';
-	}
-	
-	 
-	if(dstsize <= dstlen)
-	{
+		if(x <= dstsize - 1)
+			dst[x] = '\0';	 
+	if(dstsize < dstlen)
 	return (dstsize + srclen);
-	}
+	
 	else 
 		return(dstlen + srclen);
 }
