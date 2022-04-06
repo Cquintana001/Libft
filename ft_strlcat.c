8be9a7890591271ac0ac3a6468256e23e079c7a8
caquinta <cquintana002@gmail.com>
2022-04-06 10:28:43 +0000

    Merge branch 'main' of https://github.com/Cquintana001/Libft

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 19:19:01 by caquinta          #+#    #+#             */
-/*   Updated: 2022/04/05 11:20:58 by user             ###   ########.fr       */
+/*   Updated: 2022/04/06 10:06:05 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,23 @@ size_t ft_strlen(const char *s);
 
 int main()
 {
-	char array1[16] = "This is ";
-	char array2[] = "a potentially long string";
+	char dest[15] = "rrrrrr";
+	dest[11] = 'a';
 	
-	printf("La funcion original da: %zu\n", strlcat(array1, array2, 0));
+	printf("La funcion original da: %zu\n", strlcat(dest, "lorem", 15));
 
-	printf("array1 da: %s\n", array1);
-	printf("array2 da: %s\n", array2);
+	printf("dest da: %s\n", dest);
+	 
 	
-	char array3[16] = "This is ";
-	char array4[] = "a potentially long string";
-		
-	printf("Mi funcion da: %zu\n", ft_strlcat(array3, array4, 0));
+	char dest1[15] = "rrrrrr";
+	dest1[11] = 'a';
+	
+	printf("Mi funcion da: %zu\n", ft_strlcat(dest1, "lorem", 15));
 
-	printf("array1 da: %s\n", array1);
-	printf("array2 da: %s\n", array2);
+	printf("dest da: %s\n\n", dest1);
+
+	
+	 
 	
 
 
@@ -49,25 +51,29 @@ size_t ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize
 
 	srclen = ft_strlen(src);
 	dstlen = ft_strlen(dst);
+	printf("la longitud de src es %lu\n", srclen);
+	printf("la longitud de dst es %lu\n", dstlen);
 
-	x = 0;
+
+	x = dstlen;
 	y = 0;
-	if (dstsize != 0)
+	if (dstsize != 0 && src[y])
 	{
-		while (src[y] && x < dstsize - 1)
+		while (src[y] && x < dstsize - 1 )
 		{
 			dst[x] = src[y];
 			x++;
 			y++;
 		}
-		dst[x] = '\0';
+		if(x < dstsize - 1)
+			dst[x] = '\0';
 	}
 	
-
-	if(dstsize+ srclen < srclen + dstlen)
+	 
+	if(dstsize <= dstlen)
 	{
 	return (dstsize + srclen);
 	}
 	else 
-		return(srclen + dstlen );
+		return(dstlen + srclen);
 }
